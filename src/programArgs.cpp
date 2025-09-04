f829d8f75268aac2bda84d600bc7dd9424b1d9cf
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-09-04 15:07:46 +0100

    Update programArgs.cpp

--- src/programArgs.cpp
+++ src/programArgs.cpp
@@ -7,24 +7,24 @@ ProgramArgs ProgramArgs::parse(int argc, char** argv) {
 	ProgramArgs args;
 	if (argc == 2 && std::string(argv[1]) == "--info") {
 		displayInfo();
-        	std::exit(0);
+        std::exit(0);
 	}
 
 	if (argc < 3 || argc > 4) {
-        	throw std::runtime_error("Usage: jpws [-alt] <cover_image> <powershell_script>\n\t\bjpws --info");
-    	}
+    	throw std::runtime_error("Usage: jpws [-alt] <cover_image> <powershell_script>\n\t\bjpws --info");
+    }
 
-    	uint8_t arg_index = 1;
+    uint8_t arg_index = 1;
 
-    	if (argc == 4) {
+    if (argc == 4) {
 		if (std::string(argv[arg_index]) != "-alt") {
-            		throw std::runtime_error("Input Error: Invalid arguments. Expecting \"-alt\" as the only optional argument.");
-        	}
-        	args.lastBlockString = ArgOption::Alt;
-       		arg_index = 2;
-    	}
+        	throw std::runtime_error("Input Error: Invalid arguments. Expecting \"-alt\" as the only optional argument.");
+        }
+        args.lastBlockString = ArgOption::Alt;
+       	arg_index = 2;
+    }
 
-    	args.image_file = argv[arg_index];
-    	args.script_file = argv[++arg_index];
-    	return args;
+    args.image_file = argv[arg_index];
+    args.script_file = argv[++arg_index];
+    return args;
 }
