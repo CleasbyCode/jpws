de8b55004ac1b795c51f442f19a528b74e37bb0b
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-03-27 02:07:28 +0000

    Delete src directory

--- src/common.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <vector>
-
-using Byte   = std::uint8_t;
-using vBytes = std::vector<Byte>;
--- src/jpeg_process.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-// Strips metadata, applies EXIF orientation, and iteratively resizes
-// to eliminate comment-block close sequences "#>" (0x23, 0x3E) from the raw JPEG data.
-// Returns true if the image was modified.
-[[nodiscard]] bool ensureImageCompatible(vBytes& image_file_vec);
