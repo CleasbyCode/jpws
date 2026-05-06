a2ccc1e816535e84c747e2c9ca3160404b8bac14
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-05-06 22:11:29 +0100

    Add files via upload

---
+++ src/common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+#include <unistd.h>
+#include <vector>
+
+using Byte   = std::uint8_t;
+using vBytes = std::vector<Byte>;
+
+class UniqueFd {
+public:
+    explicit UniqueFd(int fd = -1) noexcept : fd_(fd) {}
+    ~UniqueFd() { reset(); }
+    UniqueFd(const UniqueFd&) = delete;
+    UniqueFd& operator=(const UniqueFd&) = delete;
+    UniqueFd(UniqueFd&& other) noexcept : fd_(other.release()) {}
+    UniqueFd& operator=(UniqueFd&& other) noexcept {
+        if (this != &other) reset(other.release());
+        return *this;
+    }
+    [[nodiscard]] int get() const noexcept { return fd_; }
+    [[nodiscard]] int release() noexcept { int r = fd_; fd_ = -1; return r; }
+    void reset(int new_fd = -1) noexcept { if (fd_ >= 0) ::close(fd_); fd_ = new_fd; }
+private:
+    int fd_;
+};
