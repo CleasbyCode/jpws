655215dae521b942d4dd28df38afddd82d9add7d
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-08-19 00:47:05 +0100

    Delete src directory

--- src/tests/testdata/scripts/script_block.ps1
+++
@@ -1,6 +0,0 @@
-# jpws golden-test script (block)
-$items = 1..5 | ForEach-Object { "item-$_" }
-foreach ($i in $items) {
-    Write-Output $i
-}
-Write-Host "done"
--- src/tests/testdata/scripts/script_hello.ps1
+++
@@ -1,2 +0,0 @@
-Write-Host "hello from the jpws polyglot golden test"
-Get-Date
